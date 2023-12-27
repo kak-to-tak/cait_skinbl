f8d67e6a5d893ed58a3aeddbe11dda1c8e94f6d6
kak-to-tak <154633226+kak-to-tak@users.noreply.github.com>
2023-12-27 11:51:59 +0500

    Add files via upload

---
+++ projectcaitskinbl 27.12.2023/log.php
@@ -0,0 +1,14 @@
+<?php
+include('log.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>doifkpd</h1>
+</body>
+</html>
---
+++ projectcaitskinbl 27.12.2023/registerjs.js
@@ -0,0 +1,12 @@
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+
+// let button = document.querySelector(".login-ssilka"); 
+
+// console.dir(button);
+
+// button.addEventListener("click", function() { 
+// Window.location = "index.html"; 
+// }); 
